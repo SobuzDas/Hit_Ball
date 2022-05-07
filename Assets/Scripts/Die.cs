68fd7984a431e073db2c546346270ee3c4bf5921
SobuzDas <sobujthegreat@gmail.com>
2022-05-07 23:50:06 +0600

    hit ball game

---
+++ Assets/Scripts/Die.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Die : MonoBehaviour
+
+{
+    public GameObject Bird;
+    public GameObject Pattern;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            Destroy();
+         }
+    }
+
+    private void Destroy()
+    {
+        Destroy(Bird);
+        Instantiate(Pattern, transform.position, Quaternion.identity);
+    }
+
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpringJoint2D springJoint;
+    private bool isPressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        springJoint = GetComponent<SpringJoint2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressed)
+        {
+            rb.position =Camera.main.ScreenToWorldPoint (Input.mousePosition);
+        }
+    }
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(0.15f);
+
+        GetComponent<SpringJoint2D>().enabled = false;
+
+    }
+
+
+}
